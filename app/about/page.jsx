e341bedd7c72a643d3996630498cbae0718807a9
JerryDcodemaster <ekeminijoseph83@gmail.com>
2023-09-04 11:09:07 +0100

    Initial Commit

---
+++ app/about/page.jsx
@@ -0,0 +1,21 @@
+export const metadata = {
+  title: 'About Traversy Media',
+};
+
+const AboutPage = () => {
+  return (
+    <div>
+        <h1>About Traversy Media</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+          Quis illo ex totam, veniam, nesciunt repellendus reiciendis 
+          dolorum soluta fugiat corporis, velit doloribus dolores aliquid 
+          libero fugit illum maxime autem natus minus odit nostrum aliquam 
+          voluptas. Nulla veniam quas vitae tenetur, ducimus nisi amet aliquam 
+          sint. Nisi eum placeat odit harum!
+        </p>
+    </div>
+  )
+}
+
+export default AboutPage
---
+++ app/about/team/page.jsx
@@ -0,0 +1,15 @@
+const TeamPage = () => {
+  return (
+    <div>
+      <h1>Our Team</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing 
+        elit. Blanditiis repudiandae nihil quibusdam, atque fuga, 
+        sit provident reprehenderit voluptas, commodi cumque exercitationem 
+        eos facere eaque sint perferendis ad! Voluptatibus, fugiat labore!
+      </p>
+    </div>
+  )
+}
+
+export default TeamPage
